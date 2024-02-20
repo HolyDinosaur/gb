4fb0cb487e43127488bfee882fef1ce1f006cfb1
HolyDinosaur <jsvyatoslav@gmail.com>
2024-02-20 14:20:15 +0300

    Add first home task

---
+++ Simple Algorithms/task_one.cs
@@ -0,0 +1,11 @@
+using System;
+class Program{
+	static void Main()
+{
+	Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 7 == 0 && a % 23 == 0){
+	Console.WriteLine("да");
+} else {
+	Console.WriteLine("нет");
+}}}
