c4e6ecc723ba32db7189257cd53d9b0d62efd101
HolyDinosaur <jsvyatoslav@gmail.com>
2024-03-06 22:28:19 +0300

    Homework 1,2,3

--- String/Home Work 6 Arrays and Strings/homework_3.cs
+++ String/Home Work 6 Arrays and Strings/homework_3.cs
@@ -2,4 +2,17 @@ using System;
 class Program{
         static void Main()
 {
+Console.Write("Введите значение: ");
+string str = Console.ReadLine();
+string str2 = "";
+for(int i = str.Length-1;i>=0;i--){
+	str2+=str[i];
+	}
+        if (str==str2){
+		Console.WriteLine("Введенная строка является палиндромом.");
+		}
+	else {
+		Console.WriteLine("Введенная строка не является палиндромом.");
+		
+	}
 }}
